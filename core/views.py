d88e8a7e861f597de37e92f631ae554acb00fbc5
Arafteny Maxim <maxtester2000@gmail.com>
2017-04-23 18:36:08 +0300

    Small fixes

--- core/views.py
+++ core/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
 	return render(request, 'index.html')
+
+def post_detail(request):
+	return render(request, 'post_detail.html')
